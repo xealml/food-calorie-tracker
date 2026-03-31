c7453dd113c6567106707346e736c38c046d4789
xeal <935547148@qq.com>
2026-03-31 16:21:42 +0800

    Initialize empty React Native project directory with App.js

---
+++ app/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { SafeAreaView, Text } from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Text>Hello, Food Calorie Tracker!</Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;
